31431e8f1d980a797dbd942c2233e7768f19317f
Andresa04 <152659235+Andresa04@users.noreply.github.com>
2023-12-01 19:10:05 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,5 @@
+// Marca o dia atual na pagina contato
+window.addEventListener('DOMContentLoaded', event => {
+    const listHoursArray = document.body.querySelectorAll('.list-hours li');
+    listHoursArray[new Date().getDay()].classList.add(('today'));
+})
